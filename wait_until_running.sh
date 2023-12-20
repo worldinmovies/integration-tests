11a455a26ef735bf65895cd3fea1039b10e7f6ae
daveri <david.eriksson@avanza.se>
2023-12-20 09:36:59 +0100

    asd

--- wait_until_running.sh
+++ wait_until_running.sh
@@ -9,17 +9,12 @@ function log_and_exit() {
 ##### MongoDB  #####
 curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error localhost:27017 | grep 'It looks like you are trying to access MongoDB over HTTP on the native driver port' && echo "Mongo up and running" || log_and_exit "MongoDB"
 
-#####  Redis   #####
-while ! nc -z localhost 6379 ; do echo "Redis not yet up"; sleep 1 ; done
-echo "Redis up and running";
-
 ##### RabbitMQ #####
-while ! nc -z localhost 5672 ; do echo "RabbitMQ not yet up"; sleep 1 ; done
-echo "RabbitMQ up and running";
+curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost/15672 && echo "RabbitMQ up and running" || log_and_exit "TMDB"
 
 #####   TMDB   #####
-curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL https://tmdb.localhost/status && echo "TMDB up and running" || log_and_exit "TMDB"
+curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost:8020/status && echo "TMDB up and running" || log_and_exit "TMDB"
 
 #####   Webapp   #####
-curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL https://webapp.localhost && echo "Webapp up and running" || log_and_exit "Webapp"
+curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL https://localhost && echo "Webapp up and running" || log_and_exit "Webapp"
 
