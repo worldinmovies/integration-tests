6293909f35750e9ab610d6703ef70954c4558d7b
daveri <david.eriksson@avanza.se>
2023-12-23 20:21:06 +0100

    Correct logging

--- wait_until_running.sh
+++ wait_until_running.sh
@@ -10,7 +10,7 @@ function log_and_exit() {
 curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error localhost:27017 | grep 'It looks like you are trying to access MongoDB over HTTP on the native driver port' && echo "Mongo up and running" || log_and_exit "MongoDB"
 
 ##### RabbitMQ #####
-curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost/15672 && echo "RabbitMQ up and running" || log_and_exit "TMDB"
+curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost/15672 && echo "RabbitMQ up and running" || log_and_exit "RabbitMQ"
 
 #####   TMDB   #####
 curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost:8020/status && echo "TMDB up and running" || log_and_exit "TMDB"
