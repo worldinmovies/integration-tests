047a94c297efc87cefe2f2c64bd547fe37082403
daveri <david.eriksson@avanza.se>
2024-04-07 22:17:43 +0200

    Update wait-until_running to work with a && b || c

--- wait_until_running.sh
+++ wait_until_running.sh
@@ -7,14 +7,14 @@ function log_and_exit() {
 }
 
 ##### MongoDB  #####
-curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error localhost:27017 | grep 'It looks like you are trying to access MongoDB over HTTP on the native driver port' && echo "Mongo up and running" || log_and_exit "MongoDB"
+if curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error localhost:27017 | grep 'It looks like you are trying to access MongoDB over HTTP on the native driver port'; then echo "Mongo up and running"; else log_and_exit "MongoDB"; fi
 
 ##### RabbitMQ #####
-curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost/15672 && echo "RabbitMQ up and running" || log_and_exit "RabbitMQ"
+if curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost/15672; then echo "RabbitMQ up and running"; else log_and_exit "RabbitMQ"; fi
 
 #####   TMDB   #####
-curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost:8020/status && echo "TMDB up and running" || log_and_exit "TMDB"
+if curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost:8020/status; then echo "TMDB up and running"; else log_and_exit "TMDB"; fi
 
 #####   Webapp   #####
-curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost && echo "Webapp up and running" || log_and_exit "Webapp"
+if curl --connect-timeout 10 --retry 10 --retry-max-time 60 --silent --show-error -kL http://localhost; then echo "Webapp up and running"; else log_and_exit "Webapp"; fi
 
